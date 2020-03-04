e45ca142e049d592e6ad34881083c96f775d3cc3
Dave Song <49602639+woony91@users.noreply.github.com>
2020-03-04 10:44:17 -0500

    added async function with onPostExecute and doInBackground

--- app/src/main/java/com/example/top10downloaderapplication/MainActivity.java
+++ app/src/main/java/com/example/top10downloaderapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.top10downloaderapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    private class DownloadData extends AsyncTask<String, Void, String>{
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
     }
 }
